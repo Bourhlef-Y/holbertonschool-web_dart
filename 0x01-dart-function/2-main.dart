9476fe20aee8125fa96e86d5145282cea5e0fdef
Bourhlef-Y <bourhlef.yacine53@gmail.com>
2025-07-11 12:02:33 +0200

    recorrection task 2

--- 0x01-dart-function/2-main.dart
+++ 0x01-dart-function/2-main.dart
@@ -3,6 +3,5 @@ import '2-sumfunc.dart';
 void main(List<String> args) {
   var a = int.parse(args[0]);
   var b = int.parse(args[1]);
-  print('Add ${a} + ${b} = ${add(a, b)}');
-  print('Sub ${a} - ${b} = ${sub(a, b)}');
+  print(showFunc(a, b));
 }
