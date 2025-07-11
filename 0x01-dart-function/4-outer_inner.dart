31d31b7abb3746eb07e760d329547f5bc24ff000
Bourhlef-Y <bourhlef.yacine53@gmail.com>
2025-07-11 11:58:55 +0200

    task 4 correction

--- 0x01-dart-function/4-outer_inner.dart
+++ 0x01-dart-function/4-outer_inner.dart
@@ -1,8 +1,8 @@
 void outer(String name, String id) {
   String Function() inner = () {
     List<String> names = name.split(' ');
-    String initials = '${names[0][0]}.${names[1][0]}';
-    return 'Hello Agent $initials your id is $id';
+    String initial = names[1][0];
+    return 'Hello Agent $initial.${names[0]} your id is $id';
   };
   print(inner());
 }
