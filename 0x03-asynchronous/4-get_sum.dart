af831d2ca602539d2a0d2fb573432311eafcdf11
Bourhlef-Y <bourhlef.yacine53@gmail.com>
2025-07-11 13:14:38 +0200

    task4 correction

--- 0x03-asynchronous/4-get_sum.dart
+++ 0x03-asynchronous/4-get_sum.dart
@@ -1,26 +1,22 @@
 import 'dart:convert';
 import '4-util.dart';
 
-Future<double> calculateTotal() async {
+calculateTotal() async {
+  double total = 0;
   try {
-    // Get user data and extract ID
-    String userData = await fetchUserData();
-    Map<String, dynamic> userMap = jsonDecode(userData);
-    String userId = userMap['id'];
+    String user = await fetchUserData();
+    Map<String, dynamic> userData = jsonDecode(user);
+    String id = userData['id'];
 
-    // Get user orders
-    String ordersJson = await fetchUserOrders(userId);
-    List<dynamic> userOrders = jsonDecode(ordersJson);
-
-    // Calculate total price
-    double total = 0;
-    for (String product in userOrders) {
-      String priceStr = await fetchProductPrice(product);
-      total += double.parse(priceStr);
+    String orders = await fetchUserOrders(id);
+    List<dynamic> userOrders = jsonDecode(orders);
+    for (int i = 0; i < userOrders.length; i++) {
+      total += jsonDecode(await fetchProductPrice(userOrders[i]));
     }
 
     return total;
-  } catch (error) {
-    return -1;
-  }
-} 
+
+    } catch (error) {
+      return -1;
+    }
+}
